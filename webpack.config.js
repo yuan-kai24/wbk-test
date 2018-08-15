ded0c97403194f995c04963066de162cd8b0b065
yuankai <yuan.kai.24@foxmail.com>
2018-08-15 16:48:04 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test:/\.css$/,
+				loader:'style-loader!css-loader'
+			}
+		]
+	}
+}
